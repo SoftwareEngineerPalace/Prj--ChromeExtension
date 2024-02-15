d7bdd86f4c7fd1b67f82168e0bfa7992dfdd4da2
xiaojianjun <jianjun.xiao@foxmail.com>
2024-02-15 22:03:48 +0800

    删除 dist

--- dist/background.js
+++
@@ -1,1 +0,0 @@
-console.log("background init");chrome.runtime.onMessage.addListener((a,e)=>{(async()=>a.type==="open_side_panel"&&(console.log("tabId",e.tab.id),await chrome.sidePanel.open({tabId:e.tab.id})))()});
--- dist/popup.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,R as o}from"./client.js";const r=()=>t.jsx(t.Fragment,{children:"我是 PopUp 界面"}),n=e(document.getElementById("root"));n.render(t.jsx(o.StrictMode,{children:t.jsx(r,{})}));
